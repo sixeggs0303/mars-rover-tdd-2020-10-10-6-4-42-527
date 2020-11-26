98fa779756e9edc5844bad6fda5d1b9561fa0137
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 17:05:53 +0800

    [MarsRover][Marcus Luk] feat: Added testcase [0,0,S] [M], failed.

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -33,4 +33,18 @@ class DemoTest {
         assertEquals("N", marsRover.getDirection());
     }
 
+    @Test
+    public void should_0_minus_1_E_when_executeCommands_given_0_0_S_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "E");
+        String commands = "M";
+
+        //when
+        marsRover.executeCommands(commands);
+
+        //then
+        assertEquals(0, marsRover.getXLocation());
+        assertEquals(-1, marsRover.getYLocation());
+        assertEquals("E", marsRover.getDirection());
+    }
 }
