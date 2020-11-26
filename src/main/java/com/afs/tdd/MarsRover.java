a45c7e162aea9f738c784ca6ab6164a4c7b9eab5
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 16:57:32 +0800

    [MarsRover][Marcus Luk] feat: Added test case [0,0,N] [M], failed.

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,41 @@
+package com.afs.tdd;
+
+public class MarsRover {
+    private int xLocation;
+    private int yLocation;
+    private String direction;
+
+    public MarsRover(int xLocation, int yLocation, String direction) {
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+        this.direction = direction;
+    }
+
+    public void executeCommands(String commands){
+        // Do something
+    }
+
+    public int getxLocation() {
+        return xLocation;
+    }
+
+    public void setxLocation(int xLocation) {
+        this.xLocation = xLocation;
+    }
+
+    public int getyLocation() {
+        return yLocation;
+    }
+
+    public void setyLocation(int yLocation) {
+        this.yLocation = yLocation;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,20 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    public void should_return_y_plus_1_when_executeCommands_given_0_0_N_and_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+        String commands = "M";
 
+        //when
+        marsRover.executeCommands(commands);
+
+        //then
+        assertEquals(1, marsRover.getyLocation());
     }
+
 }
