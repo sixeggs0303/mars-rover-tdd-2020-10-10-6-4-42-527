71e28185e2323cd7ac501aa03fbe1c2f917dd441
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 17:55:07 +0800

    [MarsRover][Marcus Luk] feat: Refactor codes. Split executeCommands into functions.

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -16,76 +16,74 @@ public class MarsRover {
     public void executeCommands(String commands) {
         Arrays.stream(commands.split("")).forEach(command -> {
             if (command.equals("M")) {
-                switch (this.getDirection()) {
-                    case "S":
-                        this.yLocation--;
-                        break;
-                    case "E":
-                        this.xLocation++;
-                        break;
-                    case "W":
-                        this.xLocation--;
-                        break;
-                    case "N":
-                        this.yLocation++;
-                        break;
-                }
-            } else if (command.equals("L")) {
-                switch (this.getDirection()) {
-                    case "S":
-                        this.direction = "E";
-                        break;
-                    case "E":
-                        this.direction = "N";
-                        break;
-                    case "W":
-                        this.direction = "S";
-                        break;
-                    case "N":
-                        this.direction = "W";
-                        break;
-                }
+                this.moveRover();
             } else {
-                switch (this.getDirection()) {
-                    case "S":
-                        this.direction = "W";
-                        break;
-                    case "E":
-                        this.direction = "S";
-                        break;
-                    case "W":
-                        this.direction = "N";
-                        break;
-                    case "N":
-                        this.direction = "E";
-                        break;
-                }
+                turnRover(command);
             }
         });
 
+    }
+
+    public void moveRover() {
+        switch (this.getDirection()) {
+            case "S":
+                this.yLocation--;
+                break;
+            case "E":
+                this.xLocation++;
+                break;
+            case "W":
+                this.xLocation--;
+                break;
+            case "N":
+                this.yLocation++;
+                break;
+        }
+    }
+
+    public void turnRover(String command) {
+        if (command.equals("L")) {
+            switch (this.getDirection()) {
+                case "S":
+                    this.direction = "E";
+                    break;
+                case "E":
+                    this.direction = "N";
+                    break;
+                case "W":
+                    this.direction = "S";
+                    break;
+                case "N":
+                    this.direction = "W";
+                    break;
+            }
+        } else {
+            switch (this.getDirection()) {
+                case "S":
+                    this.direction = "W";
+                    break;
+                case "E":
+                    this.direction = "S";
+                    break;
+                case "W":
+                    this.direction = "N";
+                    break;
+                case "N":
+                    this.direction = "E";
+                    break;
+            }
+        }
     }
 
     public int getXLocation() {
         return xLocation;
     }
 
-    public void setXLocation(int xLocation) {
-        this.xLocation = xLocation;
-    }
-
     public int getYLocation() {
         return yLocation;
     }
 
-    public void setYLocation(int yLocation) {
-        this.yLocation = yLocation;
-    }
-
     public String getDirection() {
         return direction;
-    }
-
-    public void setDirection(String direction) {
-        this.direction = direction;
     }
 }
