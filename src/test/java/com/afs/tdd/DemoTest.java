2d41d2f5a941088a3198577cbd789a5dabe0ef28
Marcus Luk <marcus.luk@oocl.com>
2020-11-26 17:44:01 +0800

    [MarsRover][Marcus Luk] feat: Added testcase [0,0,S] [W], failed.

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -182,4 +182,19 @@ class DemoTest {
         assertEquals(0, marsRover.getYLocation());
         assertEquals("S", marsRover.getDirection());
     }
+
+    @Test
+    public void should_0_0_N_when_executeCommands_given_0_0_W_and_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "W");
+        String commands = "R";
+
+        //when
+        marsRover.executeCommands(commands);
+
+        //then
+        assertEquals(0, marsRover.getXLocation());
+        assertEquals(0, marsRover.getYLocation());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
